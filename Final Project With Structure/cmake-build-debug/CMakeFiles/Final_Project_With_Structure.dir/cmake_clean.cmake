964d362f2ab5b7782415fdcba8ec4841f3811a1c
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2022-06-09 13:14:02 +0800

    initial commit

---
+++ Final Project With Structure/cmake-build-debug/CMakeFiles/Final_Project_With_Structure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Project_With_Structure.dir/main.c.obj"
+  "Final_Project_With_Structure.exe"
+  "Final_Project_With_Structure.exe.manifest"
+  "Final_Project_With_Structure.pdb"
+  "libFinal_Project_With_Structure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Final_Project_With_Structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
