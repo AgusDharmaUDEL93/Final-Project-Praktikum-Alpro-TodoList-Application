151f31ad317cab5c1f33cd3b2b5d9a0cdeb60eea
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2022-06-09 12:54:48 +0800

    initial commit

---
+++ Final Project Final Prak Alpro/cmake-build-debug/CMakeFiles/Final_Project_Final_Prak_Alpro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Final_Project_Final_Prak_Alpro.dir/main.c.obj"
+  "Final_Project_Final_Prak_Alpro.exe"
+  "Final_Project_Final_Prak_Alpro.exe.manifest"
+  "Final_Project_Final_Prak_Alpro.pdb"
+  "libFinal_Project_Final_Prak_Alpro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Final_Project_Final_Prak_Alpro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
